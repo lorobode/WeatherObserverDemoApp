fdf6687831f704156c851ead5fa1b6b19ebd8138
Nichollas Costa Fonseca <nichollascf@gmail.com>
2020-08-21 21:16:47 -0300

    Add action bar to MainActivity and prepare for add/select user

--- app/src/main/java/com/weatherobserverdemoapp/ui/main/viewmodel/MainViewModel.kt
+++ app/src/main/java/com/weatherobserverdemoapp/ui/main/viewmodel/MainViewModel.kt
@@ -1,14 +1,27 @@
 package com.weatherobserverdemoapp.ui.main.viewmodel
 
+import android.view.View
 import androidx.lifecycle.MutableLiveData
+import com.weatherobserverdemoapp.R
 import com.weatherobserverdemoapp.ui.base.BaseViewModel
 import javax.inject.Inject
 
 class MainViewModel @Inject constructor() : BaseViewModel() {
 
-    val helloWorld = MutableLiveData<String>()
+    val listMessage = MutableLiveData<Int>()
+    val userImage = MutableLiveData<Int>()
+    val userName = MutableLiveData<Int>()
+    val userNameVisibility = MutableLiveData<Int>()
+
+    val userLoaded = false
 
     init {
-        helloWorld.value = "Hello World!"
+        if (userLoaded) {
+
+        } else {
+            listMessage.value = R.string.select_user
+            userImage.value = R.drawable.baseline_add_circle_24
+            userNameVisibility.value = View.GONE
+        }
     }
 }
---
+++ app/src/main/java/com/weatherobserverdemoapp/utils/BindingAdapters.kt
@@ -0,0 +1,45 @@
+package com.weatherobserverdemoapp.utils
+
+import android.view.View
+import android.widget.TextView
+import androidx.appcompat.app.AppCompatActivity
+import androidx.appcompat.widget.AppCompatImageView
+import androidx.databinding.BindingAdapter
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.Observer
+import com.weatherobserverdemoapp.utils.extensions.getParentActivity
+
+@BindingAdapter("mutableReferenceImage")
+fun setMutableReferenceImage(view: AppCompatImageView, img: MutableLiveData<Int?>?) {
+    val parentActivity: AppCompatActivity? = view.getParentActivity()
+    if (parentActivity != null && img != null) {
+        img.observe(parentActivity, Observer { value ->
+            value?.let {
+                view.setImageResource(value)
+            }
+        })
+    }
+}
+
+@BindingAdapter("mutableReferenceText")
+fun setMutableReferenceText(view: TextView, text: MutableLiveData<Int>?) {
+    val parentActivity: AppCompatActivity? = view.getParentActivity()
+    if (parentActivity != null && text != null) {
+        text.observe(parentActivity, Observer { value ->
+            value?.let {
+                if (it == 0) view.text = ""
+                else view.text = parentActivity.getText(it)
+            }
+        })
+    }
+}
+
+@BindingAdapter("mutableVisibility")
+fun setMutableVisibility(view: View, visibility: MutableLiveData<Int>?) {
+    val parentActivity: AppCompatActivity? = view.getParentActivity()
+    if (parentActivity != null && visibility != null) {
+        visibility.observe(
+            parentActivity,
+            Observer { value -> view.visibility = value ?: View.VISIBLE })
+    }
+}
